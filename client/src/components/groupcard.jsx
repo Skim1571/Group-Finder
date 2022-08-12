b4ef243e2251a9d6f0726c40eb0c20047a6e0358
Johnathan Weil <weilj872@gmail.com>
2022-08-12 16:22:26 -0400

    styling nav bar

--- client/src/components/groupcard.jsx
+++ client/src/components/groupcard.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom"
 
 export default function GroupCard(props){
-const navigate = useNavigate('/groupDetails')
 return(
   <div>
     <form action='/group'>
