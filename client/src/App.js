2d692bf4286516f8bfc4ed71df003b4d6bccf3e2
Johnathan Weil <weilj872@gmail.com>
2022-08-12 10:25:58 -0400

    Basic about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
 import { Routes, Route, useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import { BASE_URL } from './globals';
-import axios from 'axios'
-
-
-import './style/App.css';
+// import { useState, useEffect } from 'react'
+// import { BASE_URL } from './globals'
+// import axios from 'axios'
+import AboutUs from './pages/about'
+import './style/App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <header className="App-header"></header>
       <main>
         <Routes>
-
+          <Route />
+          <Route path="/about" element={<AboutUs />} />
+          <Route />
         </Routes>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
