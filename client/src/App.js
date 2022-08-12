0715380e89ef7b693047b196ee790a0563eb8a37
Skim1571 <skim1571@gmail.com>
2022-08-12 08:54:40 -0500

    updated shared boiler for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { BASE_URL } from './globals';
+import axios from 'axios'
+
+
+import './style/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <main>
+        <Routes>
+
+        </Routes>
+      </main>
     </div>
   );
 }
