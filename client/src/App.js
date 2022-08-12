892bae76d9b18b9b28ada45bca26ce58638098fa
Johnathan Weil <weilj872@gmail.com>
2022-08-12 11:07:02 -0400

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route, useNavigate } from 'react-router-dom'
-
 // import { useState, useEffect } from 'react'
 // import { BASE_URL } from './globals'
 // import axios from 'axios'
 import { Home } from './pages/Home'
 import AboutUs from './pages/about'
 import './style/App.css'
+import Nav from './components/nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Nav />
+      </header>
       <main>
         <Routes>
           <Route index element={<Home />} />
--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,18 +1,16 @@
 import React from 'react'
+import Nav from '../components/nav'
 
 export default function AboutUs() {
   return (
-    <body>
-      <div>
-        <h1> About</h1>
-        <h2>Johnathan Weil, Maham Khan, Michael Pines, Steve Kim</h2>
-        <p>
-          This Website was created to help players find people to play more
-          easily. If you need people for an in game event, competitve players to
-          play ranked with, or people to play casual games with and just have
-          fun.
-        </p>
-      </div>
-    </body>
+    <div>
+      <h1> About</h1>
+      <h2>Johnathan Weil, Maham Khan, Michael Pines, Steve Kim</h2>
+      <p>
+        This Website was created to help players find people to play more
+        easily. If you need people for an in game event, competitve players to
+        play ranked with, or people to play casual games with and just have fun.
+      </p>
+    </div>
   )
 }
