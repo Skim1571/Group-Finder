d2621899e1c4a0199e67abd34401e6579060a805
Skim1571 <skim1571@gmail.com>
2022-08-19 08:39:17 -0500

    deploying to heroku

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,5 @@
-// const BASE_URL = ''
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = 'https://git.heroku.com/group-finder-api.git'
+// const BASE_URL = "http://localhost:3001";
 
 module.exports = {
   BASE_URL,
