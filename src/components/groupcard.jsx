87578088d096b1cda3c4a5a87381940057c5b809
Skim1571 <skim1571@gmail.com>
2022-08-19 11:05:57 -0500

    updated button poster css

--- src/components/groupcard.jsx
+++ src/components/groupcard.jsx
@@ -1,28 +1,31 @@
 import axios from "axios"
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import { BASE_URL } from "../globals"
 
 export default function GroupCard({ chooseGroup, group }) {
-  const[gameImage,setGameImage]=useState()
-let gameId = group.gameId
+  const [gameImage, setGameImage] = useState()
+  let gameId = group.gameId
   const handleSubmit = (event) => {
     event.preventDefault()
     chooseGroup(group)
   }
   console.log(gameId)
-useEffect(()=>{
-   const gameImage = async()=>{
+  useEffect(() => {
+    const gameImage = async () => {
       let res = await axios.get(`${BASE_URL}/api/games/image/${gameId}`)
+      console.log(res.data.image)
       setGameImage(res.data.image)
-
     }
-gameImage()
-},[])
-   
+    gameImage()
+  }, [])
+
+  console.log('image', gameImage)
+
   return (
-    <div style={{backgroundImage:`url(${gameImage})`,backgroundRepeat:"no-repeat",backgroundPosition:"center", backgroundSize:'20%'}}>
+    <div>
       <form onSubmit={(event) => handleSubmit(event)}>
         <button type="submit" className="poster">
+          <img src={gameImage} alt='image' className='cardImage' />
           <h1>Game: {group.title}</h1>
           <h3>group size: {group.groupSize}</h3>
           <h3>date: {group.date.substring(0, 10)}</h3>
--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,5 @@
-const BASE_URL = `https://group-finder-api.herokuapp.com`
-// const BASE_URL = "http://localhost:3001";
+// const BASE_URL = `https://group-finder-api.herokuapp.com`
+const BASE_URL = "http://localhost:3001";
 
 module.exports = {
   BASE_URL,
